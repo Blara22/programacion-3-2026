1f92e58127c43d34ed558fe797dc6719adbe6d61
Brenda Lara <b.lara@uabcs.mx>
2026-02-04 09:44:55 -0700

    Primera ventana hecha en Swing

---
+++ src/main/MiPanel.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class MiPanel extends JPanel{
+	
+	public MiPanel() {
+		setBackground(Color.BLUE);
+		//setBackground(new Color(210,165,35));
+	}
+	
+}
---
+++ src/main/Ventana.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame{
+	
+	public Ventana() {
+		setSize(500, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocation(100,100);
+		//setBounds(100,100,500,500);
+		setResizable(false);
+		setTitle("Mi Aplicación");
+		setLocationRelativeTo(null);
+		
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Image icono = tk.getImage("src/img/icono.png");
+		setIconImage(icono);
+		
+		MiPanel panelito = new MiPanel();
+		add(panelito);
+		
+		setVisible(true);
+		
+		//validate();
+		//repaint();
+	}
+	
+}
+
+
+
+
+
+
+
+
+
