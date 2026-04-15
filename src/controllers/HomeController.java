c566b558380d4f8c0a85e693f869783dc140ac2f
Brenda Lara <b.lara@uabcs.mx>
2026-04-15 09:43:52 -0700

    Tabla de usuarios con CardLayout

--- src/controllers/HomeController.java
+++ src/controllers/HomeController.java
@@ -9,6 +9,7 @@ import javax.swing.JOptionPane;
 
 import models.User;
 import repository.UserRepository;
+import tablemodels.UserTableModel;
 import views.LoginWindow;
 import views.MainWindow;
 
@@ -35,19 +36,29 @@ public class HomeController {
 		});
 		
 		view.btnUsers.addActionListener(e -> {
-			UserRepository repository = new UserRepository();
-			try {
-				List<User> users = repository.getUsers();
-				
-				for(User user : users) {
-					System.out.println(user);
-					System.out.println("---------------");
-				}
-			} catch (IOException ex) {
-				// TODO Auto-generated catch block
-				JOptionPane.showMessageDialog(view, ex.getMessage());
-			}
+			showUsers();
 		});
+		
+		view.btnHome.addActionListener(e -> view.showView(MainWindow.HOME));
+		
+	}
+	
+	private void showUsers() {
+		UserRepository repository = new UserRepository();
+		
+		try {
+			List<User> users = repository.getUsers();
+			
+			UserTableModel model = new UserTableModel(users);
+			
+			view.usersPanel.setTableModel(model);
+			
+			view.showView(MainWindow.USERS);
+			
+		}catch (IOException ex) {
+			JOptionPane.showMessageDialog(view, ex.getMessage());
+		}
+		
 	}
 	
 	private void handleClose() {
